454e4ac53a8d423ba920e641e66194d95e6cb821
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2023-03-06 12:38:07 -0300

    feat: add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './global.css';
+import { Checkout } from './pages/Checkout';
+import { Home } from './pages/Home';
+import { Success } from './pages/Success';
 
 function App() {
-  return <div>Hello Word</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/success" element={<Success />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
