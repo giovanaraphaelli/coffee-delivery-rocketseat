7d4934ab6542878574558aad82e888880a534528
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2023-03-06 17:44:08 -0300

    feat: add links navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,18 +1,23 @@
 import { MapPin, ShoppingCart } from 'phosphor-react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/Logo.svg';
 import styles from './styles.module.css';
 
 export function Navbar() {
   return (
     <header className={styles.header}>
-      <img src={Logo} alt="Logo Coffee Delivery" />
+      <Link to="/">
+        <img src={Logo} alt="Logo Coffee Delivery" />
+      </Link>
       <div className={styles.actions}>
         <div className={styles.location}>
           <MapPin size={32} color="#8047F8" weight="fill" />
           <span>São Paulo, SP</span>
         </div>
         <div className={styles.cart}>
-          <ShoppingCart size={32} weight="fill" color="#C47F17" />
+          <Link to="/checkout">
+            <ShoppingCart size={32} weight="fill" color="#C47F17" />
+          </Link>
         </div>
       </div>
     </header>
